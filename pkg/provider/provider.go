c5b0c375122526dbc2f43666a7d35198973e67ea
0x5487 <jawc@hotmail.com>
2025-04-06 06:31:22 +0000

    feat: add serviceDiscovery interface

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -1,8 +1,24 @@
 package provider
 
+import (
+	"context"
+	"net"
+)
+
 type ChangeFunc func() error
 
 type Provider interface {
 	Watch() error
 	SetOnChanged(ChangeFunc)
+}
+
+type ServiceDiscovery interface {
+	GetInstances(ctx context.Context, serviceName string) ([]Instance, error)
+	Watch(ctx context.Context, serviceName string) (<-chan []Instance, error)
+}
+
+type Instance interface {
+	Address() net.Addr
+	Weight() int
+	Tag(key string) (value string, exist bool)
 }
